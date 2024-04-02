bc87ba4836449393a5b4367feefc6d40aa20d1d0
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-04-03 01:15:12 +0300

    Commit 1
    Add Provider and reducer.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+  //console.log(cash)
+
+  const addCash = (cash) => {
+    dispatch({type:'ADD_CASH', payloud: cash})
+  }
+
+  const getCash = (cash) => {
+    dispatch({type:'GET_CASH',payloud: cash})
+  }
+
   return (
     <>
+      <div className='app'>
+        <div>{cash}</div>
+          <button onClick={() => addCash(Number(prompt(cash)))}>Add cash</button>
+          <button onClick={() => getCash(Number(prompt(cash)))}>Get cash</button>
+      </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const defaltState = {
+  cash: 0
+}
+
+const reducer = (state = defaltState, action) => {
+    switch (action.type) {
+      case 'ADD_CASH':
+        return {...state, cash: state.cash + action.payloud}
+      case 'GET_CASH':
+        return {...state, cash: state.cash - action.payloud}
+    
+      default:
+        return state
+    }
+}
+
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
