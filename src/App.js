d4b08d0c2cb331f992f795d61bac9aa078c93769
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-04-04 23:45:27 +0300

    Commit 3 (customerReducer)
    Add customerReducer.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const dispatch = useDispatch()
   const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
   //console.log(cash)
 
   const addCash = (cash) => {
@@ -14,13 +15,39 @@ function App() {
     dispatch({type:'GET_CASH',payloud: cash})
   }
 
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id:Date.now(),
+    }
+    dispatch({type: 'ADD_CUSTOMER', payload:  customer})
+  }
+
+  const removeCustomer = (customer) => {
+    dispatch({type: 'REMOVE_CUSTOMER', payload: customer.id })
+  }
+
   return (
     <>
       <div className='app'>
         <div>{cash}</div>
           <button onClick={() => addCash(Number(prompt(cash)))}>Add cash</button>
           <button onClick={() => getCash(Number(prompt(cash)))}>Get cash</button>
+          <button onClick={() => addCustomer(prompt())}>Add client</button>
+          <button onClick={() => getCash(Number(prompt(cash)))}>Delete client</button>
       </div>
+
+        {customers.length > 0 ?
+          <div className='customers'>
+            {customers.map(customer =>
+              <div onClick={() => removeCustomer(customer)} className='customer' key={customer.id}>{customer.name}</div>
+              )}
+          </div>
+          :
+          <div>
+            Dont have client!
+          </div>
+        }
     </>
   );
 }
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -5,9 +5,9 @@ const defaultState = {
 export const customerReducer = (state = defaultState, action) => {
     switch (action.type) {
       case 'ADD_CUSTOMER':
-        return {...state, cash: state.cash + action.payloud}
-      case 'GET_CUSTOMER':
-        return {...state, cash: state.cash - action.payloud}
+        return {...state, customers: [...state.customers, action.payload]}
+      case 'REMOVE_CUSTOMER':
+        return {...state, customers: state.customers.filter(customer => customer.id !== action.payload)}
     
       default:
         return state
